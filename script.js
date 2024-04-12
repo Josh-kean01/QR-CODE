1eb47058aced8fcb27b231130c6557271c047153
Adekunle Joshua <adekunlej960@gmail.com>
2024-04-12 06:56:32 +0100

    updated

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ generateBtn.addEventListener("click", () => {
   generateBtn.innerText = "Generating QR Code...";
   qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
   qrDownload.href = qrImg.src
+  qrDownload.setAttribute("download", "qr-code");
   qrImg.addEventListener("load", () => {
     wrapper.classList.add("active");
     generateBtn.innerText = "Generate QR Code";
